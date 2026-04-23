976726a845ba577018cdf370d49d0d0de66391b5
Maksym Boiko <maksym782@gmail.com>
2026-04-23 12:23:32 +0200

    feat(http): implement base response in JwtMiddleware, addjustments in SpaceMiddleware

--- src/shared/adapters/JwtGuard.hpp
+++ src/shared/adapters/JwtGuard.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "shared/adapters/JwtAuthenticatorService.hpp"
+#include "shared/adapters/http/response/HttpResponses.hpp"
 
 #include "crow.h"
 #include <utility>
@@ -16,30 +17,7 @@ struct JwtMiddleware : public crow::ILocalMiddleware
     /** @brief Rejects request when JWT header is missing or invalid. */
     void before_handle(crow::request &req,
                        crow::response &res,
-                       context &ctx)
-    {
-        const auto auth = req.get_header_value("Authorization");
-        if (auth.size() >= 8 && auth.rfind("Bearer ", 0) == 0)
-        {
-            auto user_id = JwtAuthenticatorService::validate_Jwt(auth.substr(7));
-            if (user_id.has_value())
-            {
-                ctx.user_id = std::move(user_id.value());
-            }
-            else
-            {
-                res.code = 401;
-                res.end();
-                return;
-            }
-        }
-        else
-        {
-            res.code = 401;
-            res.end();
-            return;
-        }
-    }
+                       context &ctx);
 
     void after_handle(crow::request &, crow::response &, context &) {}
 };
